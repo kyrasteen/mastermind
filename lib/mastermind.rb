2219ff2197ce391ec9158120b2a426e1f86a8843
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-01 10:06:54 -0700

    implemented only four color method

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,22 +1,86 @@
+require_relative 'messeges'
+
 class Mastermind
 
-  attr_reader :messeges, :entry
+  attr_reader :messages, :game_in_progress, :colors, :match
+  attr_accessor :secret
 
-  def initialize(messeges)
-    @messeges = messeges
-  end
-
-  def play_game(command)
-
+  def initialize(messages)
+    @messages = messages
+    @game_in_progress = false
   end
 
   def execute(input)
+
+    if !game_in_progress
+      game_menu(input)
+    else
+      play_game(input)
+    end
+
     # secret = "BBGB"
     # if input == secret
     #   "You win!"
     # else
     #   "Guess again!"
     # end
+
+    if input == 'q'
+      return [messages.end_round, :continue]
+    end
+  end
+
+  def game_menu(input)
+    if input == 'p'
+      @secret = ['R','G','B','Y'].shuffle
+      @game_in_progress = true
+      [messages.take_guess, :continue]
+    elsif input == 'q'
+      [messages.quit_game, :stop]
+    end
+  end
+
+  def play_game(input)
+    if input == 'q'
+      return [messages.end_round, :continue]
+    end
+
+    #what if entry is more than four items?
+    if input.length > 4
+      return [messages.too_many, :continue]
+    end
+    #if an el of input has a letter not representing a color
+    #array, return message and continue
+    guesses = input.map do |i|
+      i.include?('Q')
+    end
+
+    if guesses[2] == true
+      return [messages.invalid_letter, :continue]
+    end
+
+
+    guess = input#.split('')
+    guess = guess.map { |letter| letter.upcase}
+    matched = @secret.zip(guess)
+
+    match = 0
+     matched.each do |a|
+      if a[0] == a[1]
+        match += 1 #can my other class see colors?
+      end
+      [messages.exact_match, :continue]
+    end
+
+    #if guess includes a letter that entry includes
+    #return correct color message
+    colors = guess.find_all { |letter| @secret.include?(letter)} #what does this return?
+      if colors.length > 0
+        [messages.color_match, :continue]
+        match = colors.length
+      end
+
+
   end
 
 end
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,12 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/mastermind'
+
+require '../lib/mastermind'
 
 class MastermindTest < Minitest::Test
 
+  attr_reader :mm
+
   def setup
-    messeges = Messeges.new
-    @mm = Mastermind.new(messeges)
+    messages = Messages.new
+    @mm = Mastermind.new(messages)
   end
 
   def test_it_exists
@@ -17,16 +20,57 @@ class MastermindTest < Minitest::Test
 #to the four letters in entry requirement
 
   def test_it_starts_a_game
-    assert @mm.entry.nil?
+    assert @mm.secret.nil?
     @mm.execute('p')
-    assert @mm.entry.is_a?(String)  #string or array?
+    assert @mm.secret.is_a?(Array)  #string or array?
   end
 
-  def test_it_only_takes_four_letters_in_entry
-    assert
+  def test_it_generates_random_four_letters
+    #not passing no message given
+    @mm.execute('p')
+    puzzle = @secret
+    messages = Messages.new
+    master = Mastermind.new(messages)
+    master.execute('p')
+    puzzler = @secret
+    assert puzzle != puzzler
+  end
+
+  def test_it_only_takes_four_letters_in_guess
+    @mm.execute('p')
+    @mm.secret = ['R','G','G','B']
+    message, signal = @mm.execute(['Y','B','B','G','y'])
+    assert message.include?("Too many")
+    assert_equal signal, :continue
+  end
+
+  def test_it_only_accepts_four_color_choices_in_guess
+    @mm.execute('p')
+    message, signal = @mm.execute(['y','G','Q','r'])
+    assert message.include?("Invalid")
+    assert_equal signal, :continue
+  end
+
+  def test_it_ends_a_game
+    message, signal = @mm.execute('q')
+    assert message.include?('Thanks')
+    assert_equal signal, :stop
+  end
+
+  def test_it_ends_a_round
+    skip
+  end
+
+  def test_guess_includes_correct_colors
+    skip
+  end
+
+  def test_matching_colors_in_guess_at_correct_place
+    skip
   end
 
   def test_it_wins
+    skip
     mm = Mastermind.new
     result = mm.execute("BBGB")
     assert result.downcase.include?("win")
