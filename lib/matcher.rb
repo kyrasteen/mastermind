abb4ca8e3841690d6042ba393bb37005d013e358
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-06 15:42:58 -0700

    Add Matcher, #correct?, #correct_colors
    Adds Matcher class which accepts two strings (guess, secret) to initialize,
    and current has methods for correct?, and correct_colors.  The idea is to
    accept simple input (strings), and return simple output (true/false, integers
    for correct_colors and correct_positions).

---
+++ lib/matcher.rb
@@ -0,0 +1,28 @@
+class Matcher
+  attr_reader :guess, :secret
+
+  def initialize(guess, secret)
+    @guess  = guess.upcase
+    @secret = secret.upcase
+  end
+
+  def correct?
+    guess == secret
+  end
+
+  def correct_colors
+    colors = guess.upcase.split('').find_all { |letter|
+      # require 'pry'; binding.pry
+      secret.include?(letter) }
+    colors.uniq.length
+    # @guess_count += 1
+
+    # if colors.length >= 2
+    #   "'#{input}' has #{colors.length} correct colors."
+    # elsif colors.length == 1
+    #   "'#{input}' has #{colors.length} correct color."
+    # else
+    #   "'#{input}' has no colors guessed correctly. Really? Try again!"
+    # end
+  end
+end
