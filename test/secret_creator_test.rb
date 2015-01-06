39287b78f090e17f9c04a7a8dfe31b5dd2bd8876
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-06 14:06:00 -0700

    Add tests for SecretCreator
    Originally had some unecessary duplication (creating a whole new SecretCreator
    object to generate a second sequence).

---
+++ test/secret_creator_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/secret_creator'
+
+class SecretCreatorTest < Minitest::Test
+
+  def setup
+    @sc = SecretCreator.new
+  end
+
+  def test_it_exists
+    assert @sc
+  end
+
+  def test_it_generates_random_four_letters_each_round
+    code = @sc.generate_secret
+    code_two = @sc.generate_secret
+    assert code != code_two
+  end
+
+  def test_it_has_four_letters
+    code = @sc.generate_secret
+    assert_equal code.length, 4
+  end
+end
