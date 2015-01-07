dc80f51d71dd22d588464cef51c60d3dd56196d9
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-06 19:14:01 -0700

    Removed unecessary conditionals with strings

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -14,18 +14,8 @@ class Matcher
     colors = guess.chars.find_all { |letter| secret.include?(letter) }
     colors.uniq.length
     # @guess_count += 1
-
-    # if colors.length >= 2
-    #   "'#{input}' has #{colors.length} correct colors."
-    # elsif colors.length == 1
-    #   "'#{input}' has #{colors.length} correct color."
-    # else
-    #   "'#{input}' has no colors guessed correctly. Really? Try again!"
-    # end
   end
 
-  # similar to correct_colors, return an integer of
-  # the guess' correct positons
   def correct_positions
     matched = secret.chars.zip(guess.chars)
     match = matched.select { |a| a[0] == a[1] }
